77daf285a5b9ef145ab24a742bf6ccf85a346a9a
Christof Ruch <christof.ruch@gmx.de>
2022-03-09 11:23:40 +0100

    Implement a trivial wrapper plugin that runs the latency measurement implemented in the MTDM class continuously. Expose the measurement value as AudioPlugin Parameters, which is a bit of a misuse as they are really output only.

--- Plugins/JuceDelayPlugin/Source/PluginProcessor.cpp
+++ Plugins/JuceDelayPlugin/Source/PluginProcessor.cpp
@@ -1,13 +1,65 @@
 #include "PluginProcessor.h"
 
+#include "mtdm.h"
+
+MinimalAudioPlugin::MinimalAudioPlugin()
+    : sampleRate_(48000.0)
+    , samplesPerBlock_(128)
+{
+    delayInSamples_ = new juce::AudioParameterFloat ("delay", "Delay in Samples", 0.0f, 50000.0f, 0.0f);
+    delayInMS_ = new juce::AudioParameterFloat("delay", "Delay in ms", 0.0f, 1000.0f, 0.0f);
+    noSignalDetected_ =new juce::AudioParameterBool("noSignal", "No Signal present", true);
+    invertPhaseActive_ = new juce::AudioParameterBool  ("invertPhase", "Invert Phase", false);
+    errorConditionTrue_ = new juce::AudioParameterBool("errorCondition", "Error Condition", false);
+
+    addParameter(delayInSamples_);
+    addParameter(delayInMS_);
+    addParameter(noSignalDetected_);
+    addParameter(invertPhaseActive_);
+    addParameter(errorConditionTrue_);
+}
+
 void MinimalAudioPlugin::processBlock(juce::AudioBuffer<float>& buffer,
                                       juce::MidiBuffer& midiMessages)
 {
     juce::ignoreUnused(midiMessages);
-    buffer.clear();
+    if (buffer.getNumChannels() > 0) {
+        // Only process the first channel
+        mtdm_->process(buffer.getNumSamples(), buffer.getReadPointer(0), buffer.getWritePointer(0));
+        detectLatency(); // I think we can affort to do this at packet rate. The original code sleeps 250ms between calculations
+    }
+}
+
+void MinimalAudioPlugin::prepareToPlay(double sampleRate, int samplesPerBlock)
+{
+    mtdm_ = std::make_unique<MTDM>(static_cast<int>(round(sampleRate)));
+    sampleRate_ = sampleRate;
+    samplesPerBlock_ = samplesPerBlock;
 }
 
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new MinimalAudioPlugin();
+}
+
+void MinimalAudioPlugin::detectLatency()
+{
+    if (mtdm_->resolve() < 0)
+    {
+        *noSignalDetected_ = true;
+    }
+    else
+    {
+        *noSignalDetected_ = false;
+        if (mtdm_->err() > 0.35f)
+        {
+            mtdm_->invert();
+            mtdm_->resolve();
+        }
+        *delayInSamples_ = mtdm_->del();
+        *errorConditionTrue_ = (mtdm_->err() > 0.30f);
+        *invertPhaseActive_ = mtdm_->inv();
+        double t = 1000.0 / sampleRate_;
+        *delayInMS_ = delayInSamples_->get() * t;
+    }
 }
--- Plugins/JuceDelayPlugin/Source/PluginProcessor.h
+++ Plugins/JuceDelayPlugin/Source/PluginProcessor.h
@@ -5,9 +5,31 @@
 //Inhereting from PluginHelpers::ProcessorBase, which is just inhereting from juce::AudioProcessor
 //And adding some default implementations
 
+class MTDM;
+
 class MinimalAudioPlugin : public PluginHelpers::ProcessorBase
 {
 public:
+    MinimalAudioPlugin();
+
     void processBlock(juce::AudioBuffer<float>& buffer,
                       juce::MidiBuffer& midiMessages) override;
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+
+private:
+    void detectLatency();
+
+    std::unique_ptr<MTDM> mtdm_;
+    double sampleRate_;
+    int samplesPerBlock_; 
+
+    juce::AudioParameterFloat* delayInSamples_;
+    juce::AudioParameterFloat* delayInMS_;
+    juce::AudioParameterBool* noSignalDetected_;
+    juce::AudioParameterBool* invertPhaseActive_;
+    juce::AudioParameterBool* errorConditionTrue_;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MinimalAudioPlugin)
 };
+
